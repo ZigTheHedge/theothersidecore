e772857ad14df617a6e4abc16157b6493f6bdabd
ZigTheHedge <junk@cwelth.com>
2019-08-25 20:33:09 +0300

    Derp with dimID

--- src/main/java/com/cwelth/theothersidecore/ModMain.java
+++ src/main/java/com/cwelth/theothersidecore/ModMain.java
@@ -29,7 +29,7 @@ public class ModMain {
 
     public static final String NAME = "The Other Side modpack Core Mod";
     public static final String MODID = "theothersidecore";
-    public static final String VERSION = "1.02";
+    public static final String VERSION = "1.03";
     public static final CreativeTabs toscCreativeTab = new TOSCCreativeTab();
 
 
